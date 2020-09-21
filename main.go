e4792cab1bb3eb1d83d98b4c4fdb65a3c7517ab5
laven <mailofzt@126.com>
2020-09-21 14:06:44 +0800

    offer04

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import "dailyLeetCode/offer"
 
-func main()  {
-	offer.TestOffer03()
+func main() {
+	//fmt.Println(offer.GetRandPage(7*24*3600))
+	//fmt.Println(2818785 /3600/24)
+	offer.TestOffer4()
 }
--- offer/offer04.go
+++ offer/offer04.go
@@ -1,8 +1,13 @@
 package offer
+
+import (
+	"fmt"
+)
+
 /*
 在一个 n * m 的二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
 
- 
+
 
 示例:
 
@@ -19,7 +24,7 @@ package offer
 
 给定 target = 20，返回 false。
 
- 
+
 
 限制：
 
@@ -31,3 +36,37 @@ package offer
 链接：https://leetcode-cn.com/problems/er-wei-shu-zu-zhong-de-cha-zhao-lcof
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 */
+
+func TestOffer4() {
+	a := [][]int{0: {5}, 1: {6}}
+	fmt.Println(findNumberIn2DArray(a, 6))
+	return
+}
+
+/*
+执行用时：32 ms, 在所有 Go 提交中击败了67.98%的用户
+内存消耗：6.6 MB, 在所有 Go 提交中击败了33.62%的用户
+*/
+func findNumberIn2DArray(matrix [][]int, target int) bool {
+	if l1 := len(matrix); l1 <= 0 || l1 > 1000 {
+		return false
+	}
+	l2 := len(matrix[0])
+	if l2 <= 0 || l2 > 1000 {
+		return false
+	}
+	for _, v := range matrix {
+		if target < v[0] || target > v[l2-1] {
+			continue
+		}
+		for _, vv := range v {
+			if vv > target {
+				break
+			}
+			if vv == target {
+				return true
+			}
+		}
+	}
+	return false
+}
